891ce556b7bc58f71fc47352611a8fca35343434
Sam Collins <samc3259@gmail.com>
2022-05-05 05:26:59 +0100

    documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,131 @@
+//! Graph traversal functions using common search algorithms.
+//!
+//! Abstracts away the details of the search, allowing you to focus solely on the problem-specific details.
+//!
+//! Traversals visit each node once.
+//!
+//! Works on infinite graphs.
+//!
+//! The functions return an iterator over states, in visit order.
+//! A state should include the current node in the graph, along with any necessary search-specific data such as the total cost to reach the node.
+//!
+//! To turn a traversal into a search, use `.find(goal)`, where `goal` is a predicate specifying the target node.
+//! You can also `.filter(goal)` to obtain an iterator over any states (or nodes) satisfying the predicate.
+//!
+//! Each traversal function requires at least the following:
+//!
+//! * An initial state
+//! * A function for generating all states adjacent to a given state
+//! * A function for normalising a state, returning a unique identifier of the node it represents
+//!   * Must return a type that is [`Eq`] and [`Hash`]
+//!   * Should remove any search-specific data, such as total cost to reach the node
+//!
+//! When the length, or total cost, of the path to a target node is desired, and when `dijkstra` or `a_star` are being used,
+//! it is often useful to keep track of the total cost to reach the node within the state itself.
+//!
+//! Similarly, when the full path from start to finish is desired, it is often useful to keep track of the nodes previously visited within the state.
+//!
+//! Data such as this should be removed when a state is normalised.
+//!
+//! # Examples
+//!
+//! ```
+//! use search::bft;
+//! use std::collections::HashMap;
+//!
+//! // To traverse a graph of this form with a breadth-first search:
+//! // a --- b --- c
+//! // | \       / |
+//! // |  \     /  |
+//! // |   \   /   |
+//! // d --- e     f
+//! let graph = HashMap::from([
+//!     ('a', vec!['b', 'd', 'e']),
+//!     ('b', vec!['a', 'c']),
+//!     ('c', vec!['b', 'e', 'f']),
+//!     ('d', vec!['a', 'e']),
+//!     ('e', vec!['a', 'c', 'd']),
+//!     ('f', vec!['c']),
+//! ]);
+//!
+//! struct State {
+//!     node: char,
+//!     path: Vec<char>,
+//! }
+//!
+//! impl State {
+//!     fn adjacent(&self, graph: &HashMap<char, Vec<char>>) -> Vec<State> {
+//!         let mut states = Vec::new();
+//!         for &node in &graph[&self.node] {
+//!             let mut path = self.path.clone();
+//!             path.push(node);
+//!             states.push(State { node, path });
+//!         }
+//!         states
+//!     }
+//! }
+//!
+//! let mut traverse = bft(
+//!     State { node: 'a', path: vec!['a'] },
+//!     |s| s.adjacent(&graph),
+//!     |s| s.node,
+//! );
+//! let goal = traverse.find(|s| s.node == 'f').unwrap();
+//!
+//! assert_eq!(goal.node, 'f');
+//! assert_eq!(goal.path, vec!['a', 'b', 'c', 'f']);
+//! ```
+//!
+//! ```
+//! use search::dijkstra;
+//! use std::collections::HashMap;
+//!
+//! // To traverse a graph of this form with Dijkstra's algorithm:
+//! // a -6- b -3- c
+//! // | \       / |
+//! // 1  5     4  7
+//! // |   \   /   |
+//! // d -2- e     f
+//! let graph = HashMap::from([
+//!     ('a', vec![('b', 6), ('d', 1), ('e', 5)]),
+//!     ('b', vec![('a', 6), ('c', 3)]),
+//!     ('c', vec![('b', 3), ('e', 4), ('f', 7)]),
+//!     ('d', vec![('a', 1), ('e', 2)]),
+//!     ('e', vec![('a', 5), ('c', 4), ('d', 2)]),
+//!     ('f', vec![('c', 7)]),
+//! ]);
+//!
+//! struct State {
+//!     node: char,
+//!     cost: u8,
+//!     path: Vec<char>,
+//! }
+//!
+//! impl State {
+//!     fn adjacent(&self, graph: &HashMap<char, Vec<(char, u8)>>) -> Vec<State> {
+//!         let mut states = Vec::new();
+//!         for &(node, weight) in &graph[&self.node] {
+//!             let mut path = self.path.clone();
+//!             path.push(node);
+//!             states.push(State { node, cost: self.cost + weight, path });
+//!         }
+//!         states
+//!     }
+//! }
+//!
+//! let mut traverse = dijkstra(
+//!     State { node: 'a', cost: 0, path: vec!['a'] },
+//!     |s| s.adjacent(&graph),
+//!     |s| s.node,
+//!     |s| s.cost,
+//! );
+//! let goal = traverse.find(|s| s.node == 'f').unwrap();
+//!
+//! assert_eq!(goal.node, 'f');
+//! assert_eq!(goal.cost, 14);
+//! assert_eq!(goal.path, vec!['a', 'd', 'e', 'c', 'f']);
+//! ```
+
 use std::{
     cmp::Ordering,
     collections::{BinaryHeap, HashSet, VecDeque},
@@ -61,7 +189,9 @@ where
     }
 }
 
-/// A function for traversing a graph with a breadth-first traversal, visiting each node once.
+/// A function for traversing a graph with a [breadth-first](https://en.wikipedia.org/wiki/Breadth-first_search) traversal.
+///
+/// Returns an iterator over states, in visit order.
 ///
 /// Requires:
 /// * An initial state
@@ -92,6 +222,7 @@ where
 /// ]);
 ///
 /// let mut traverse = bft('a', |x| (&graph[x]).clone(), |x| *x);
+///
 /// assert_eq!(traverse.next(), Some('a'));
 /// assert_eq!(traverse.next(), Some('b'));
 /// assert_eq!(traverse.next(), Some('d'));
@@ -102,15 +233,59 @@ where
 ///
 /// // To find the length of the shortest path to a target node:
 ///
-/// let length = bft(
-///     ('a', 0),
-///     |&(x, cost)| graph[&x].clone().into_iter().map(move |y| (y, cost + 1)),
-///     |(x, _)| *x,
-/// )
-/// .find(|(x, _)| *x == 'f')
-/// .unwrap()
-/// .1;
-/// assert_eq!(length, 3);
+/// struct State {
+///     node: char,
+///     cost: u8,
+/// }
+///
+/// impl State {
+///     fn adjacent(&self, graph: &HashMap<char, Vec<char>>) -> Vec<State> {
+///         let mut states = Vec::new();
+///         for &node in &graph[&self.node] {
+///             states.push(State { node, cost: self.cost + 1 });
+///         }
+///         states
+///     }
+/// }
+///
+/// let mut traverse = bft(
+///     State { node: 'a', cost: 0 },
+///     |s| s.adjacent(&graph),
+///     |s| s.node,
+/// );
+/// let goal = traverse.find(|s| s.node == 'f').unwrap();
+///
+/// assert_eq!(goal.node, 'f');
+/// assert_eq!(goal.cost, 3);
+///
+/// // To find the shortest path to a target node:
+///
+/// struct PathState {
+///     node: char,
+///     path: Vec<char>,
+/// }
+///
+/// impl PathState {
+///     fn adjacent(&self, graph: &HashMap<char, Vec<char>>) -> Vec<PathState> {
+///         let mut states = Vec::new();
+///         for &node in &graph[&self.node] {
+///             let mut path = self.path.clone();
+///             path.push(node);
+///             states.push(PathState { node, path });
+///         }
+///         states
+///     }
+/// }
+///
+/// let mut traverse = bft(
+///     PathState { node: 'a', path: vec!['a'] },
+///     |s| s.adjacent(&graph),
+///     |s| s.node,
+/// );
+/// let goal = traverse.find(|s| s.node == 'f').unwrap();
+///
+/// assert_eq!(goal.node, 'f');
+/// assert_eq!(goal.path, vec!['a', 'b', 'c', 'f']);
 /// ```
 pub fn bft<S, AF, A, NF, N>(start: S, adjacent: AF, normalise: NF) -> impl Iterator<Item = S>
 where
@@ -122,7 +297,9 @@ where
     Traverse::new(start, Queue::new(), adjacent, normalise)
 }
 
-/// A function for traversing a graph with a depth-first traversal, visiting each node once.
+/// A function for traversing a graph with a [depth-first](https://en.wikipedia.org/wiki/Depth-first_search) traversal.
+///
+/// Returns an iterator over states, in visit order.
 ///
 /// Requires:
 /// * An initial state
@@ -153,6 +330,7 @@ where
 /// ]);
 ///
 /// let mut traverse = dft('a', |x| graph[x].clone(), |x| *x);
+///
 /// assert_eq!(traverse.next(), Some('a'));
 /// assert_eq!(traverse.next(), Some('e'));
 /// assert_eq!(traverse.next(), Some('d'));
@@ -160,6 +338,35 @@ where
 /// assert_eq!(traverse.next(), Some('f'));
 /// assert_eq!(traverse.next(), Some('b'));
 /// assert_eq!(traverse.next(), None);
+///
+/// // To find a path to a target node:
+///
+/// struct State {
+///     node: char,
+///     path: Vec<char>,
+/// }
+///
+/// impl State {
+///     fn adjacent(&self, graph: &HashMap<char, Vec<char>>) -> Vec<State> {
+///         let mut states = Vec::new();
+///         for &node in &graph[&self.node] {
+///             let mut path = self.path.clone();
+///             path.push(node);
+///             states.push(State { node, path });
+///         }
+///         states
+///     }
+/// }
+///
+/// let mut traverse = dft(
+///     State { node: 'a', path: vec!['a'] },
+///     |s| s.adjacent(&graph),
+///     |s| s.node,
+/// );
+/// let goal = traverse.find(|s| s.node == 'f').unwrap();
+///
+/// assert_eq!(goal.node, 'f');
+/// assert_eq!(goal.path, vec!['a', 'e', 'c', 'f']);
 /// ```
 pub fn dft<S, AF, A, NF, N>(start: S, adjacent: AF, normalise: NF) -> impl Iterator<Item = S>
 where
@@ -171,9 +378,11 @@ where
     Traverse::new(start, Stack::new(), adjacent, normalise)
 }
 
-/// A function for traversing a weighted graph using Dijkstra's algorithm, visiting each node once.
+/// A function for traversing a weighted graph using [Dijkstra's algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm).
 ///
-/// It is often useful to store the total cost required to reach the node within the state itself.
+/// Returns an iterator over states, in visit order.
+///
+/// It is often useful to keep track of the total cost to reach the node within the state itself.
 ///
 /// Requires:
 /// * An initial state
@@ -206,42 +415,80 @@ where
 ///     ('f', vec![('c', 7)]),
 /// ]);
 ///
+/// #[derive(PartialEq, Debug)]
+/// struct State {
+///     node: char,
+///     cost: u8,
+/// }
+///
+/// impl State {
+///     fn adjacent(&self, graph: &HashMap<char, Vec<(char, u8)>>) -> Vec<State> {
+///         let mut states = Vec::new();
+///         for &(node, weight) in &graph[&self.node] {
+///             states.push(State { node, cost: self.cost + weight });
+///         }
+///         states
+///     }
+/// }
+///
 /// let mut traverse = dijkstra(
-///     ('a', 0), // initial state
-///     |&(x, cost)| { // adjacent states function
-///         graph[&x]
-///             .clone()
-///             .into_iter()
-///             .map(move |(y, w)| (y, cost + w))
-///     },
-///     |(x, _)| *x, // normalise function
-///     |(_, cost)| *cost, // cost function
+///     State { node: 'a', cost: 0 },
+///     |s| s.adjacent(&graph),
+///     |s| s.node,
+///     |s| s.cost,
 /// );
-/// assert_eq!(traverse.next(), Some(('a', 0)));
-/// assert_eq!(traverse.next(), Some(('d', 1)));
-/// assert_eq!(traverse.next(), Some(('e', 3)));
-/// assert_eq!(traverse.next(), Some(('b', 6)));
-/// assert_eq!(traverse.next(), Some(('c', 7)));
-/// assert_eq!(traverse.next(), Some(('f', 14)));
+///
+/// assert_eq!(traverse.next(), Some(State { node: 'a', cost: 0 }));
+/// assert_eq!(traverse.next(), Some(State { node: 'd', cost: 1 }));
+/// assert_eq!(traverse.next(), Some(State { node: 'e', cost: 3 }));
+/// assert_eq!(traverse.next(), Some(State { node: 'b', cost: 6 }));
+/// assert_eq!(traverse.next(), Some(State { node: 'c', cost: 7 }));
+/// assert_eq!(traverse.next(), Some(State { node: 'f', cost: 14 }));
 /// assert_eq!(traverse.next(), None);
 ///
-/// // To search for a target node:
+/// // To find the cost of the shortest path to a target node:
 ///
-/// let cost = dijkstra(
-///     ('a', 0),
-///     |&(x, cost)| {
-///         graph[&x]
-///             .clone()
-///             .into_iter()
-///             .map(move |(y, w)| (y, cost + w))
-///     },
-///     |(x, _)| *x,
-///     |(_, cost)| *cost,
-/// )
-/// .find(|(x, _)| *x == 'f')
-/// .unwrap()
-/// .1;
-/// assert_eq!(cost, 14);
+/// let mut traverse = dijkstra(
+///     State { node: 'a', cost: 0 },
+///     |s| s.adjacent(&graph),
+///     |s| s.node,
+///     |s| s.cost,
+/// );
+/// let goal = traverse.find(|s| s.node == 'f').unwrap();
+///
+/// assert_eq!(goal.node, 'f');
+/// assert_eq!(goal.cost, 14);
+///
+/// // To find the shortest path to a target node:
+///
+/// struct PathState {
+///     node: char,
+///     cost: u8,
+///     path: Vec<char>,
+/// }
+///
+/// impl PathState {
+///     fn adjacent(&self, graph: &HashMap<char, Vec<(char, u8)>>) -> Vec<PathState> {
+///         let mut states = Vec::new();
+///         for &(node, weight) in &graph[&self.node] {
+///             let mut path = self.path.clone();
+///             path.push(node);
+///             states.push(PathState { node, cost: self.cost + weight, path });
+///         }
+///         states
+///     }
+/// }
+///
+/// let mut traverse = dijkstra(
+///     PathState { node: 'a', cost: 0, path: vec!['a'] },
+///     |s| s.adjacent(&graph),
+///     |s| s.node,
+///     |s| s.cost,
+/// );
+/// let goal = traverse.find(|s| s.node == 'f').unwrap();
+///
+/// assert_eq!(goal.node, 'f');
+/// assert_eq!(goal.path, vec!['a', 'd', 'e', 'c', 'f']);
 /// ```
 pub fn dijkstra<S, AF, A, NF, N, CF, P>(
     start: S,
@@ -260,9 +507,11 @@ where
     Traverse::new(start, PriorityQueue::new(cost), adjacent, normalise)
 }
 
-/// A function for traversing a weighted graph using A* search algorithm, visiting each node once.
+/// A function for traversing a weighted graph using [A* search](https://en.wikipedia.org/wiki/A*_search_algorithm) algorithm.
 ///
-/// It is often useful to store the total cost required to reach the node within the state itself.
+/// Returns an iterator over states, in visit order.
+///
+/// It is often useful to keep track of the total cost to reach the node within the state itself.
 ///
 /// Requires:
 /// * An initial state
@@ -292,23 +541,39 @@ where
 /// let obstacles = HashSet::from([(0, 0), (2, 1), (3, 1), (4, 3)]);
 /// let goal = (4, 0);
 ///
-/// let steps = a_star(
-///     (0i8, 4i8, 0i8), // initial state
-///     |&(x, y, cost)| { // adjacent states function
-///         [(1, 0), (0, -1), (-1, 0), (0, 1)]
-///             .into_iter()
-///             .map(move |(x1, y1)| (x + x1, y + y1))
-///             .filter(|pos| !obstacles.contains(pos))
-///             .map(move |(x, y)| (x, y, cost + 1))
-///     },
-///     |(x, y, _)| (*x, *y), // normalise function
-///     |(_, _, cost)| *cost, // cost function
-///     |(x, y, _)| (goal.0 - *x).abs() + (goal.1 - *y).abs(), // heuristic function
-/// )
-/// .find(|(x, y, _)| (*x, *y) == goal)
-/// .unwrap()
-/// .2;
-/// assert_eq!(steps, 8);
+/// struct State {
+///     x: i8,
+///     y: i8,
+///     cost: u8,
+/// }
+///
+/// impl State {
+///     fn adjacent(&self, obstacles: &HashSet<(i8, i8)>) -> Vec<State> {
+///         let mut states = Vec::new();
+///         for offset in [(1, 0), (0, -1), (-1, 0), (0, 1)] {
+///             let (x, y) = (self.x + offset.0, self.y + offset.1);
+///             if !obstacles.contains(&(x, y)) {
+///                 states.push(State { x, y, cost: self.cost + 1 });
+///             }
+///         }
+///         states
+///     }
+///
+///     fn heuristic(&self, goal: (i8, i8)) -> u8 {
+///         self.x.abs_diff(goal.0) + self.y.abs_diff(goal.1)
+///     }
+/// }
+///
+/// let mut traverse = a_star(
+///     State { x: 0, y: 4, cost: 0 },
+///     |s| s.adjacent(&obstacles),
+///     |s| (s.x, s.y),
+///     |s| s.cost,
+///     |s| s.heuristic(goal),
+/// );
+/// let goal = traverse.find(|s| (s.x, s.y) == goal).unwrap();
+///
+/// assert_eq!(goal.cost, 8);
 /// ```
 pub fn a_star<S, AF, A, NF, N, CF, HF, P>(
     start: S,
