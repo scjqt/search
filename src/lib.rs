7bb3def4cd13215d8512178db668cb6f419e7cfc
Sam Collins <samc3259@gmail.com>
2022-12-12 20:22:10 +0000

    satisfy clippy

--- src/lib.rs
+++ src/lib.rs
@@ -165,7 +165,7 @@ where
                 self.states.push(state);
             }
         }
-        return Some(current);
+        Some(current)
     }
 }
 
@@ -686,7 +686,7 @@ where
     P: Ord,
 {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(&other))
+        Some(self.cmp(other))
     }
 }
 
